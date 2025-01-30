95363da292222eaaf9369c6d8e242d4e5b226c2d
Hdaydich <abbassihend91@gmail.com>
2025-01-30 09:20:07 +0100

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ export function Header({ buttonText, buttonAction }) {
 
   return (
     <div className={`row ${s.container}`}>
-      <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 ${s.divLogo}">
+      <div className={s.divLogo}>
         <Logo
           onClick={() => navigate("/")}
           subtitle="Manage your notes"
